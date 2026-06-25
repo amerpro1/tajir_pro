bb518faf52b7d72e78ccca620e3eeb71403cb145
amerpro1 <amer16.express@gmail.com>
2026-06-26 01:50:06 +0300

    Add files via upload

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "293012579"
+    serviceWorkerVersion: "3250132675"
   }
 });
